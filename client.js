9f7784a90397a680f796aad993ed6e1bb3d19207
zolwer88 <zolwer999not@gmail.com>
2026-02-13 23:26:32 -0600

    Update WebSocket server IP address

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // Replace SERVER_IP with your ZeroTier IP
-let ws = new WebSocket("ws://10.130.85.163:8765");
+let ws = new WebSocket("ws://10.130.85.53:8765");
 
 ws.onopen = () => {
     console.log("Connected to server!");
